f4abc37b9a3fdc30176ca0bb9d0c3b2afcbb8b14
Jeff Zhang <zhangjeff2014@gmail.com>
2022-12-04 23:40:32 -0500

    start ui changes

--- fridge-app-web/src/components/Sidebar.tsx
+++ fridge-app-web/src/components/Sidebar.tsx
@@ -16,63 +16,45 @@ import theme from "../theme";
 import axios from "axios";
 
 const Sidebar = () => {
-  // TODO: Switch to axios
   const logout = () => {
     axios.post("/auth/logout").then((data) => (window.location.href = "/"));
-    // fetch("/auth/logout", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    // }).then((res) => {
-    //   window.location.href = "/";
-    // });
   };
+
   return (
-    <Paper
-      sx={{
-        width: theme.spacing(30),
-        height: "100vh",
-        bgcolor: "background.paper",
-        position: "fixed",
-      }}
-    >
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Kitchen />
-            </ListItemIcon>
-            <ListItemText primary="My Fridge" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Restaurant />
-            </ListItemIcon>
-            <ListItemText primary="Saved Recipes" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <CalendarMonth />
-            </ListItemIcon>
-            <ListItemText primary="Meal Planner" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton onClick={logout}>
-            <ListItemIcon>
-              <Logout />
-            </ListItemIcon>
-            <ListItemText primary="Log out" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Paper>
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <Kitchen />
+          </ListItemIcon>
+          <ListItemText primary="My Fridge" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <Restaurant />
+          </ListItemIcon>
+          <ListItemText primary="Saved Recipes" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <CalendarMonth />
+          </ListItemIcon>
+          <ListItemText primary="Meal Planner" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton onClick={logout}>
+          <ListItemIcon>
+            <Logout />
+          </ListItemIcon>
+          <ListItemText primary="Log out" />
+        </ListItemButton>
+      </ListItem>
+    </List>
   );
 };
 
