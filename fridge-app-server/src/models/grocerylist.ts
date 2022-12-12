d0c617b2175369d35806f1fb67892c2506d81a7d
Jeff Zhang <zhangjeff2014@gmail.com>
2022-12-11 22:08:50 -0500

    grocery list backend

---
+++ fridge-app-server/src/models/grocerylist.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+export interface GroceryItemDocument {
+    id: number;
+    name: string;
+    image: string;
+    category: string;
+    possibleUnits: string[];
+    quantity: number;
+    unit: string;
+}
+
+const GroceryItemSchema = new Schema<GroceryItemDocument>({
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    possibleUnits: [String],
+    quantity: { type: Number, required: true },
+    unit: { type: String, required: true },
+})
+
+export interface GroceryListDocument {
+    userId: Types.ObjectId,
+    items: GroceryItemDocument[];
+}
+
+const GroceryListSchema = new Schema<GroceryListDocument>({
+    userId: Schema.Types.ObjectId,
+    items: { type: [GroceryItemSchema], required: true }
+})
+
+const groceryListModel = mongoose.model<GroceryListDocument>("GroceryList", GroceryListSchema, "grocery_list");
+export default groceryListModel;
