1f30ebfa20ed0d8c9eac2322bb9d3c9eefde3563
Jeff Zhang <zhangjeff2014@gmail.com>
2022-12-31 13:29:08 -0400

    Rework expiration

--- server/src/utils/duration-to-ms.ts
+++ server/src/utils/duration-to-ms.ts
@@ -1,27 +1,26 @@
 export const durationToMs = (duration: string) => {
-    // y = year
-    // m = month
-    // w = week
-    // d = day
-    // h = hour
+  // y = year
+  // m = month
+  // w = week
+  // d = day
+  // h = hour
 
-    // Input duration string must be a number followed by the unit (no spaces)
-    // Ex: 24d => 24 days
+  // Input duration string must be a number followed by the unit (no spaces)
+  // Ex: 24d => 24 days
 
-    const amount = parseInt(duration.slice(0, duration.length - 1));
-    const unit = duration[duration.length - 1];
+  const amount = parseInt(duration.slice(0, duration.length - 1));
+  const unit = duration[duration.length - 1];
 
-    switch (unit) {
-        case "y":
-            return amount * 365 * 24 * 60 * 60 * 1000;
-        case "m":
-            return amount * 30 * 24 * 60 * 60 * 1000;
-        case "w":
-            return amount * 7 * 24 * 60 * 60 * 1000;
-        case "d":
-            return amount * 24 * 60 * 60 * 1000;
-        case "h":
-            return amount * 60 * 60 * 1000;
-    }
-
-}
+  switch (unit) {
+    case "y":
+      return amount * 365 * 24 * 60 * 60 * 1000;
+    case "m":
+      return amount * 30 * 24 * 60 * 60 * 1000;
+    case "w":
+      return amount * 7 * 24 * 60 * 60 * 1000;
+    case "d":
+      return amount * 24 * 60 * 60 * 1000;
+    case "h":
+      return amount * 60 * 60 * 1000;
+  }
+};
