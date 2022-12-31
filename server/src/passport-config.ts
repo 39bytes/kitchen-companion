c7ebfeae07ae9c1d0fac71b68f11af7827000e95
Jeff Zhang <zhangjeff2014@gmail.com>
2022-12-31 14:13:28 -0400

    switch to mongo store for sessions

--- server/src/passport-config.ts
+++
@@ -1,6 +0,0 @@
-import passportLocal from 'passport-local';
-import passport, { PassportStatic, use } from 'passport';
-import bcrypt from 'bcrypt'
-import User, { UserDocument } from './models/user';
-
-
